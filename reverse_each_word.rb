02de75656005183032faf6b6c34bdeb4f3cb757a
Logan Wohlers <lawohlers@gmail.com>
2019-02-04 16:39:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,14 @@ def reverse_each_word (s)
     
   return b.join(" ")
 end
+
+
+def reverse_each_word (s)
+  a=s.split(" ")
+  b=[]
+  a.each do |s|
+    b.push(s.reverse)
+  end
+    
+  return b.join(" ")
+end
